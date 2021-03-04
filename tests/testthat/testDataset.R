32f20771475f27b78c3ebfafe844682697e12724
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-03-04 14:58:15 +0100

    Add a few tests.

--- tests/testthat/testDataset.R
+++ tests/testthat/testDataset.R
@@ -80,7 +80,7 @@ test_that("Export using config", {
   
 })
 
-test_that("Export constant covariates works well (N=1, N=2)", {
+test_that("Export constant covariates work well (N=1, N=2)", {
   
   dataset <- new("dataset") 
   
@@ -120,4 +120,94 @@ test_that("Export constant covariates works well (N=1, N=2)", {
   
 })
 
+test_that("Export fixed covariates work well (N=3)", {
+
+  arm <- new("arm", id=as.integer(1), subjects=as.integer(3))
+  
+  # Add doses
+  arm <- arm %>% add(new("bolus", time=0, amount=100))
+  arm <- arm %>% add(new("bolus", time=24, amount=100))
+  arm <- arm %>% add(new("bolus", time=48, amount=100))
+  
+  # Add covariate
+  arm <- arm %>% add(new("fixed_covariate", name="WT", values=c(65, 70, 75)))
+  arm <- arm %>% add(new("fixed_covariate", name="HT", values=c(175, 180, 185)))
+  
+  # Add observations
+  for (t in seq(0, 48, by=10)) {
+    arm <- arm %>% add(new("observation", time=t))
+  }
+  
+  dataset <- new("dataset")
+  dataset <- dataset %>% add(arm)
+  
+  # Export to RxODE N=1
+  config <- new("config", default_depot_cmt=as.integer(1), default_obs_cmt=as.integer(2))
+  table <- dataset %>% export(dest="RxODE",
+                              config=config)
+  
+  subTable <- table %>% dplyr::select(ID, WT, HT) %>% dplyr::distinct()
+  expect_equal(subTable, data.frame(ID=c(1,2,3), WT=c(65,70,75), HT=c(175,180,185)))
+})
+
+
+test_that("Export function covariates work well (N=3)", {
+  
+  arm <- new("arm", id=as.integer(1), subjects=as.integer(3))
+  
+  # Add doses
+  arm <- arm %>% add(new("bolus", time=0, amount=100))
+  arm <- arm %>% add(new("bolus", time=24, amount=100))
+  arm <- arm %>% add(new("bolus", time=48, amount=100))
+  
+  # Add covariate
+  arm <- arm %>% add(new("function_covariate", name="WT", fun="rnorm", args=list(mean=70, sd=10)))
+  arm <- arm %>% add(new("function_covariate", name="HT", fun="rnorm", args=list(mean=180, sd=20)))
+  
+  # Add observations
+  for (t in seq(0, 48, by=10)) {
+    arm <- arm %>% add(new("observation", time=t))
+  }
+  
+  dataset <- new("dataset")
+  dataset <- dataset %>% add(arm)
+  
+  # Export to RxODE N=1
+  set.seed(1)
+  config <- new("config", default_depot_cmt=as.integer(1), default_obs_cmt=as.integer(2))
+  table <- dataset %>% export(dest="RxODE", config=config)
+  
+  subTable <- table %>% dplyr::select(ID, WT, HT) %>% dplyr::distinct() %>% dplyr::mutate(WT=round(WT), HT=round(HT))
+  expect_equal(subTable, data.frame(ID=c(1,2,3), WT=c(64,72,62), HT=c(212,187,164)))
+})
+
+test_that("Export boostrap covariates work well (N=8)", {
+  
+  arm <- new("arm", id=as.integer(1), subjects=as.integer(8))
+  
+  # Add doses
+  arm <- arm %>% add(new("bolus", time=0, amount=100))
+  arm <- arm %>% add(new("bolus", time=24, amount=100))
+  arm <- arm %>% add(new("bolus", time=48, amount=100))
+  
+  # Add covariate
+  arm <- arm %>% add(new("bootstrap_covariate", name="WT", data=c(65, 70, 75), random=TRUE, replacement=TRUE))
+  arm <- arm %>% add(new("bootstrap_covariate", name="HT", data=c(175, 180, 185), random=TRUE, replacement=TRUE))
+  
+  # Add observations
+  for (t in seq(0, 48, by=10)) {
+    arm <- arm %>% add(new("observation", time=t))
+  }
+  
+  dataset <- new("dataset")
+  dataset <- dataset %>% add(arm)
+  
+  # Export to RxODE N=1
+  set.seed(1)
+  config <- new("config", default_depot_cmt=as.integer(1), default_obs_cmt=as.integer(2))
+  table <- dataset %>% export(dest="RxODE", config=config)
+  
+  subTable <- table %>% dplyr::select(ID, WT, HT) %>% dplyr::distinct()
+  expect_equal(subTable, data.frame(ID=c(1,2,3,4,5,6,7,8), WT=c(65,75,65,70,65,75,75,70), HT=c(180,185,185,175,175,175,180,180)))
+})
 
