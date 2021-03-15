c837e8c175eb425949d3e6202e92bd6e98878fab
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-03-15 12:17:55 +0100

    ALAG1/F1 IOV tests.

--- tests/testthat/testSimulateIOV.R
+++ tests/testthat/testSimulateIOV.R
@@ -78,13 +78,54 @@ test_that("Simulate IOV on F1", {
   # Simulate just IIV
   results1 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
   results1$ARM <- "IIV"
+  regressionTest(dataset, model, seed=seed, filename="3_boluses_iiv_f1.csv")
   
   # Simulate just IIV + IOV
   results2 <- model_iov %>% simulate(dataset, dest="RxODE", seed=seed)
   results2$id <- results2$id + dataset %>% length()
   results2$ARM <- "IIV + IOV"
+  regressionTest(dataset, model_iov, seed=seed, filename="3_boluses_iiv_iov_f1.csv")
   
   spaguettiPlot(rbind(results1, results2), "CP", "ARM")
   shadedPlot(rbind(results1, results2), "CP", "ARM")
+})
+
+test_that("Simulate IOV on ALAG1", {
+  # Model with IIV on ALAG1
+  model <- getNONMEMModelTemplate(4,4)
+  pk <- model@model %>% getByName("PK")
+  model@parameters <- model@parameters %>% add(Theta("ALAG1", index=6, value=5))
+  model@parameters <- model@parameters %>% add(Omega("ALAG1", index=6, index2=6, value=0.2^2))
   
+  # Model with IIV and IOV on ALAG1
+  model_iov <- model
+  iovCvPc <- 20 # 20% CV
+  
+  # Add IOV
+  model@parameters <- model@parameters %>% add(Omega("IOV_ALAG1", index=7, index2=7, value=(0/100)^2))
+  model_iov@parameters <- model_iov@parameters %>% add(Omega("IOV_ALAG1", index=7, index2=7, value=(iovCvPc/100)^2))
+  
+  dataset <- Dataset(10)
+  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Bolus(time=24, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Bolus(time=48, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Observations(times=seq(0,72, by=0.5)))
+  dataset <- dataset %>% add(IOV(colname="IOV_ALAG1_COL", distribution=EtaDistribution(omega="IOV_ALAG1")))
+  
+  # Add bioavailability
+  dataset <- dataset %>% add(LagTime(compartment=1, ParameterDistribution(theta="ALAG1", omega="ALAG1", iov="IOV_ALAG1_COL")))
+  
+  # Simulate just IIV
+  results1 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
+  results1$ARM <- "IIV"
+  regressionTest(dataset, model, seed=seed, filename="3_boluses_iiv_alag1.csv")
+  
+  # Simulate just IIV + IOV
+  results2 <- model_iov %>% simulate(dataset, dest="RxODE", seed=seed)
+  results2$id <- results2$id + dataset %>% length()
+  results2$ARM <- "IIV + IOV"
+  regressionTest(dataset, model_iov, seed=seed, filename="3_boluses_iiv_iov_alag1.csv")
+  
+  spaguettiPlot(rbind(results1, results2), "CP", "ARM")
+  shadedPlot(rbind(results1, results2), "CP", "ARM")
 })
