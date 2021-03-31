4744e1eea41b7f582667124cdd9767833b6ee41c
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-03-31 16:05:40 +0200

    Review bioavailability tests.

--- tests/testthat/testSimulateBioavailability.R
+++ tests/testthat/testSimulateBioavailability.R
@@ -9,7 +9,7 @@ seed <<- 1
 
 source(paste0(testFolder, "testUtils.R"))
 
-test_that("Simulate a bolus, 2 arms", {
+test_that("Simulate a bolus, 2 arms, F1 only in arm1, in dataset", {
   model <- getNONMEMModelTemplate(4,4)
   
   arm1 <- Arm(1, subjects=10)
@@ -32,4 +32,46 @@ test_that("Simulate a bolus, 2 arms", {
   expect_false(dataset %>% hasModelDistribution())
   
   datasetRegressionTest(dataset, model, seed=seed, filename="bolus_2arms_bioavailability")
+})
+
+test_that("Simulate a simple bolus with bioavailability", {
+  model <- getNONMEMModelTemplate(4,4)
+  regFilename <- "simple_bolus_bioavailability"
+  
+  dataset <- Dataset(3)
+  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Observations(times=seq(0,24, by=0.5)))
+  
+  # Bioavailability implemented in dataset
+  dataset <- dataset %>% add(Bioavailability(compartment=1, distribution=ConstantDistribution(0.75)))
+  
+  results1 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
+  spaguettiPlot(results1, "CP")
+  expect_equal(nrow(results1), 49 * dataset %>% length())
+  
+  results2 <- model %>% simulate(dataset, dest="mrgsolve", seed=seed)
+  spaguettiPlot(results2, "CP")
+  expect_equal(nrow(results2), 49 * dataset %>% length())
+  
+  datasetRegressionTest(dataset, model, seed=seed, filename=regFilename)
+  outputRegressionTest(results1, output="CP", filename=regFilename)
+  outputRegressionTest(results2, output="CP", filename=regFilename)
+  
+  # Bioavailability implemented in model
+  model <- model %>% add(CompartmentBioavailability(compartment=1, rhs="0.75"))
+  
+  dataset <- Dataset(3)
+  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Observations(times=seq(0,24, by=0.5)))
+  
+  results3 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
+  spaguettiPlot(results3, "CP")
+  expect_equal(nrow(results3), 49 * dataset %>% length())
+  
+  results4 <- model %>% simulate(dataset, dest="mrgsolve", seed=seed)
+  spaguettiPlot(results4, "CP")
+  expect_equal(nrow(results4), 49 * dataset %>% length())
+  
+  outputRegressionTest(results3, output="CP", filename=regFilename)
+  outputRegressionTest(results4, output="CP", filename=regFilename)
 })
