526c6e7568e8ed347914d717bb0a929f0cb7ca09
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-04-07 09:26:07 +0200

    Remove pi.Rd.

--- tests/testthat/testSimulateVarcov.R
+++ tests/testthat/testSimulateVarcov.R
@@ -17,13 +17,27 @@ test_that("Visualise median uncertainty", {
   ds <- ds %>% add(Observations(times=seq(0, 7*24)))
   results <- model %>% simulate(dataset=ds, dest="RxODE", replicates=10, output=c("id", "time", "Y"))
   
-  summary <- results %>% group_by(replicate, time) %>% summarise(median=median(Y))
-  summary <- summary %>% group_by(time) %>% summarise(med.low=quantile(median, 0.05),
-                                                      med.med=quantile(median, 0.50),
-                                                      med.up=quantile(median, 0.95))
+  summary.med <- results %>% group_by(replicate, time) %>% summarise(metric=median(Y), .groups='drop')
+  summary.med <- summary.med %>% group_by(time) %>% summarise(low=quantile(metric, 0.05),
+                                                              med=quantile(metric, 0.50),
+                                                              up=quantile(metric, 0.95))
+  
+  summary.pLo <- results %>% group_by(replicate, time) %>% summarise(metric=quantile(Y, 0.05), .groups='drop')
+  summary.pLo <- summary.pLo %>% group_by(time) %>% summarise(low=quantile(metric, 0.05),
+                                                              med=quantile(metric, 0.50),
+                                                              up=quantile(metric, 0.95))
+  
+  summary.pUp <- results %>% group_by(replicate, time) %>% summarise(metric=quantile(Y, 0.95), .groups='drop')
+  summary.pUp <- summary.pUp %>% group_by(time) %>% summarise(low=quantile(metric, 0.05),
+                                                              med=quantile(metric, 0.50),
+                                                              up=quantile(metric, 0.95))
 
-  ggplot(summary, aes(x=time, y=med.med)) +
-    geom_line(color="blue") +
-    geom_ribbon(aes(ymin=med.low, ymax=med.up), alpha=0.3)
+  ggplot(summary.med, aes(x=time, y=med)) +
+    geom_line(color="red", size=0.7) +
+    geom_ribbon(aes(ymin=low, ymax=up), alpha=0.15, color=NA, fill="red") +
+    geom_line(data=summary.pLo, color="red", lty="dashed", size=0.7) +
+    geom_ribbon(aes(ymin=low, ymax=up), data=summary.pLo, alpha=0.15, color=NA, fill="blue") +
+    geom_line(data=summary.pUp, color="red", lty="dashed", size=0.7) +
+    geom_ribbon(aes(ymin=low, ymax=up), data=summary.pUp, alpha=0.15, color=NA, fill="blue")
   
 })
