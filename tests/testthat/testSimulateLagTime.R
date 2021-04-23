c7a1a0c523c412459b3e9d032f02eb90aeee76f1
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-04-23 10:38:01 +0200

    Repair testSimulateLagTime.R part 2.

--- tests/testthat/testSimulateLagTime.R
+++ tests/testthat/testSimulateLagTime.R
@@ -9,17 +9,13 @@ seed <<- 1
 
 source(paste0(testFolder, "testUtils.R"))
 
-test_that("Simulate a bolus with lag time in dataset", {
+test_that("Simulate a bolus with fixed lag time in dataset", {
   model <- getNONMEMModelTemplate(4,4)
-  regFilename <- "bolus_lag_time_dataset"
+  regFilename <- "bolus_fixed_lag_time"
   
   dataset <- Dataset(3)
-  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1, lag=2)) # 2 hours
   dataset <- dataset %>% add(Observations(times=seq(0,24, by=0.5)))
-  
-  # 2 hours lag time with 20% CV
-  lag <- TreatmentLagTime(compartment=1, FunctionDistribution(fun="rlnorm", args=list(meanlog=log(2), sdlog=0.2)))
-  dataset <- dataset %>% add(lag)
   
   results1 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
   spaguettiPlot(results1, "CP")
@@ -29,14 +25,14 @@ test_that("Simulate a bolus with lag time in dataset", {
   spaguettiPlot(results2, "CP")
   expect_equal(nrow(results2), dataset %>% length() * 49)
   
-  datasetRegressionTest(dataset, model, seed=seed, filename=regFilename)
+  datasetRegressionTest(dataset, model, seed=seed, filename=paste0(regFilename, "_dataset"))
   outputRegressionTest(results1, output="CP", filename=regFilename)
   outputRegressionTest(results2, output="CP", filename=regFilename)
 })
 
-test_that("Simulate a bolus with lag time in model", {
+test_that("Simulate a bolus with fixed lag time in model", {
   model <- getNONMEMModelTemplate(4,4)
-  regFilename <- "bolus_lag_time_model"
+  regFilename <- "bolus_fixed_lag_time"
   
   dataset <- Dataset(3)
   dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
@@ -53,7 +49,7 @@ test_that("Simulate a bolus with lag time in model", {
   spaguettiPlot(results2, "CP")
   expect_equal(nrow(results2), dataset %>% length() * 49)
   
-  datasetRegressionTest(dataset, model, seed=seed, filename=regFilename)
+  datasetRegressionTest(dataset, model, seed=seed, filename=paste0(regFilename, "_model"))
   outputRegressionTest(results1, output="CP", filename=regFilename)
   outputRegressionTest(results2, output="CP", filename=regFilename)
 })
