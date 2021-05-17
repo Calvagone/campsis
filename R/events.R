abb910de9bc0a12c18f4fb72acba23627bca90c2
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-05-17 14:22:34 +0200

    #4 Add getTimes vector to events list.

--- R/events.R
+++ R/events.R
@@ -20,3 +20,24 @@ setClass(
 Events <- function() {
   return(new("events"))
 }
+
+#_______________________________________________________________________________
+#----                             getTimes                                  ----
+#_______________________________________________________________________________
+
+#' Get all distinct times across the events list.
+#' 
+#' @param object any object
+#' @return numeric vector with all unique times, sorted
+#' @export
+getTimes <- function(object) {
+  stop("No default function is provided")
+}
+
+setGeneric("getTimes", function(object) {
+  standardGeneric("getTimes")
+})
+
+setMethod("getTimes", signature = c("events"), definition = function(object) {
+  return(object@list %>% purrr::map(.f=~.x@times) %>% purrr::flatten_dbl() %>% unique() %>% base::sort())
+})
--- tests/testthat/testEvents.R
+++ tests/testthat/testEvents.R
@@ -6,11 +6,20 @@ context("Test all methods from the events class")
 test_that("Add, length, contains, getByName methods work as expected", {
   events <- Events()
   
-  event1 <- Event(times=5, fun=function(id, time) {
+  event1 <- Event(name="Event 1", times=5, fun=function(id, time) {
     # Do something
   })
   
-  events <- events %>% add(event1)
+  event2 <- Event(name="Event 2", times=c(5,10), fun=function(id, time) {
+    # Do something
+  })
   
-  expect_equal(events %>% length(), 1)
+  # Add both events to list
+  events <- events %>% add(event1) %>% add(event2)
+  
+  expect_equal(events %>% length(), 2)
+  
+  # Retrieve event times
+  times <- events %>% getTimes()
+  expect_equal(times, c(5,10))
 })
