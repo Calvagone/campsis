c86ab4c780af368f531f3f0940ec50b7d4906f18
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-06-15 09:34:55 +0200

    #20 NOCB/LOCF switch, add first test.

---
+++ tests/testthat/testSimulateNocbLocf.R
@@ -0,0 +1,52 @@
+library(testthat)
+library(pmxmod)
+
+context("Test the simulate method with the NOCB/LOCF switch")
+
+overwriteNonRegressionFiles <<- FALSE
+testFolder <<- ""
+seed <- 1
+
+source(paste0(testFolder, "testUtils.R"))
+
+test_that("Weight as a time-varying covariate (NOCB vs LOCF)", {
+  model <- model_library$advan4_trans4
+  model <- model %>% replaceEquation("CL", paste0(model %>% getEquation("CL"), "*pow(BW/70, 0.75)"))
+  
+  dataset <- Dataset(4)
+  dataset <- dataset %>% add(Bolus(time=0, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Bolus(time=24, amount=1000, compartment=1))
+  dataset <- dataset %>% add(Observations(times=c(0,3,9,16,24,26,48)))
+
+  # Left join time-varying BW column
+  table <- dataset %>% export(dest="RxODE", model=model, seed=seed) # CAREFUL, SEED NEEDED FOR REPRODUCIBILITY
+  weight <- data.frame(TIME=c(0,3,9,16,24,26,48), BW=c(150,20,100,250,40,170,10))
+  table <- table %>% dplyr::left_join(weight, by="TIME")
+  
+  # Dataset non-regression test
+  datasetRegressionTest(dataset, model, seed=seed, filename="wt_as_time_varying_cov")
+  
+  # LOCF tests
+  regFilename <- "wt_as_time_varying_cov_locf"
+  
+  results1 <- model %>% simulate(table, dest="RxODE", seed=seed)
+  spaghettiPlot(results1, "CP")
+
+  results2 <- model %>% simulate(table, dest="mrgsolve", seed=seed, declare="BW")
+  spaghettiPlot(results2, "CP")
+
+  outputRegressionTest(results1, output="CP", filename=regFilename)
+  outputRegressionTest(results2, output="CP", filename=regFilename)
+  
+  # NOCB tests
+  regFilename <- "wt_as_time_varying_cov_nocb"
+  
+  results1 <- model %>% simulate(table, dest="RxODE", seed=seed, nocb=TRUE)
+  spaghettiPlot(results1, "CP")
+  
+  results2 <- model %>% simulate(table, dest="mrgsolve", seed=seed, declare="BW", nocb=TRUE)
+  spaghettiPlot(results2, "CP")
+  
+  outputRegressionTest(results1, output="CP", filename=regFilename)
+  outputRegressionTest(results2, output="CP", filename=regFilename)
+})
