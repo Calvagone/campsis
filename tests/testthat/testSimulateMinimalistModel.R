b0d6bf7037ce748a374073be723316379a1e6ac8
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-08-25 15:18:50 +0200

    Add a minimalist test to cover recent changes in campsismod.

---
+++ tests/testthat/testSimulateMinimalistModel.R
@@ -0,0 +1,26 @@
+library(testthat)
+
+context("Test the simulate method with minimalist examples")
+
+overwriteNonRegressionFiles <<- FALSE
+testFolder <<- ""
+seed <- 1
+
+source(paste0(testFolder, "testUtils.R"))
+
+test_that("Simulate a minimalist model (no parameter files, no IIV & error model)", {
+  regFilename <- "minimalist_model"
+  model <- suppressWarnings(read.campsis(paste0(testFolder, "models/", regFilename)))
+  
+  dataset <- Dataset()
+  dataset <- dataset %>% add(Observations(times=seq(0,24, by=0.5)))
+  
+  results1 <- model %>% simulate(dataset, dest="RxODE", seed=seed)
+  results2 <- model %>% simulate(dataset, dest="mrgsolve", seed=seed)
+
+  spaghettiPlot(results1, "A_CENTRAL")
+  spaghettiPlot(results2, "A_CENTRAL")
+  
+  outputRegressionTest(results1, output="A_CENTRAL", filename=regFilename)
+  outputRegressionTest(results2, output="A_CENTRAL", filename=regFilename)
+})
