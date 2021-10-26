fe3e5f73e8290f4e0e2eb10d1fdabe338f53ec27
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-10-26 11:24:43 +0200

    #50 Update package version to 1.0.0.

--- tests/testthat/testDistribution.R
+++ tests/testthat/testDistribution.R
@@ -7,7 +7,7 @@ test_that("Normal distribution", {
   dist <- NormalDistribution(mean=5, sd=1)
   dist <- dist %>% sample(n=as.integer(100))
   values <- dist@sampled_values
-  hist(values)
+  #hist(values)
   expect_equal(round(mean(values), 1), 5.1)
   expect_equal(round(sd(values), 1), 0.9)
 })
@@ -17,7 +17,7 @@ test_that("Log-normal distribution", {
   dist <- LogNormalDistribution(meanlog=log(5), sdlog=0.20)
   dist <- dist %>% sample(n=as.integer(100))
   values <- dist@sampled_values
-  hist(values)
+  #hist(values)
   expect_equal(round(mean(log(values)), 1), 1.6)
   expect_equal(round(sd(log(values)), 1), 0.2)
 })
@@ -27,7 +27,7 @@ test_that("Uniform distribution", {
   dist <- UniformDistribution(min=5, max=25)
   dist <- dist %>% sample(n=as.integer(100))
   values <- dist@sampled_values
-  hist(values)
+  #hist(values)
   expect_equal(round(mean(values), 1), 15.4)
 })
 
@@ -36,7 +36,7 @@ test_that("Discrete distribution", {
   dist <- DiscreteDistribution(x=c(1,2,3), prob=c(10,20,40))
   dist <- dist %>% sample(n=as.integer(100))
   values <- dist@sampled_values
-  hist(values)
+  #hist(values)
   expect_equal(sum(values==1), 13)
   expect_equal(sum(values==2), 32)
   expect_equal(sum(values==3), 55)
