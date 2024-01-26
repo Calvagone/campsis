dd0be9625f2cd6afb89397cf62b8aad5e68aed70
luyckxn <nicolas.luyckx@calvagone.com>
2024-01-26 16:42:47 +0100

    #134 Make NHANES data available in package.

---
+++ R/data.R
@@ -0,0 +1,15 @@
+
+#' NHANES database (demographics and body measure data combined, from 2017-2018).
+#'
+#' @format data frame
+#' \describe{
+#'    \item{ID}{Original identifier}
+#'    \item{SEX}{Sex: 1 for males, 2 for females}
+#'    \item{AGE}{Age in years}
+#'    \item{BW}{Body weight in kg}
+#'    \item{BMI}{Body mass index}
+#'    \item{HT}{Height in cm}
+#' }
+#' @source \url{https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DEMO_J.XPT}
+#' @source \url{https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/BMX_J.XPT}
+"nhanes"
---
+++ data-raw/nhanes_database.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(tidyr)
+library(foreign)
+
+url <- "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/DEMO_J.XPT"
+tf <- tempfile()
+download.file(url, tf, mode="wb", quiet=TRUE)
+demo <- read.xport(tf)
+
+url <- "https://wwwn.cdc.gov/Nchs/Nhanes/2017-2018/BMX_J.XPT"
+tf <- tempfile()
+download.file(url, tf, mode="wb", quiet=TRUE)
+bmx <- read.xport(tf)
+
+nhanes.demo <- demo %>%
+  select(ID=SEQN, SEX=RIAGENDR, AGE=RIDAGEYR) %>% 
+  mutate(SEXC=case_when(SEX=='1' ~ 'male',
+                        SEX=='2' ~ 'female',
+                        SEX=='.' ~'missing'))
+nhanes.body <- bmx %>%
+  select(ID=SEQN, BW=BMXWT, BMI=BMXBMI, HT=BMXHT)
+
+# Join both tables and keep only complete cases
+nhanes <- left_join(nhanes.demo, nhanes.body) %>%
+  tidyr::drop_na()
+
+# For now, don't use the SEXC column since Bootstrap in Campsis expects numeric columns
+nhanes <- nhanes %>%
+  select(-SEXC)
+
+usethis::use_data(nhanes, overwrite=TRUE)
