c31122a70f493bd84465efb40868f4202e263691
luyckxn <nicolas.luyckx@calvagone.com>
2024-02-12 12:51:17 +0100

    #136 Don't raise exception when 2 boluses/infusions are given at same time.

--- R/arm.R
+++ R/arm.R
@@ -132,7 +132,25 @@ setMethod("add", signature = c("arm", "list"), definition = function(object, x) 
 })
 
 setMethod("add", signature = c("arm", "treatment_entry"), definition = function(object, x) {
-  object@protocol@treatment <- object@protocol@treatment %>% add(x) 
+  existing <- object@protocol@treatment %>% find(x)
+  if (is.null(existing)) {
+    object@protocol@treatment <- object@protocol@treatment %>% add(x) 
+  } else {
+    oldAmount <- existing@amount
+    xAmount <- x@amount
+    existing@amount <- 0
+    x@amount <- 0
+    if (isTRUE(all.equal(existing, x))) {
+      # Combine old amount and new amount
+      existing@amount <- oldAmount + xAmount
+      # Replace old element
+      object@protocol@treatment <- object@protocol@treatment %>% replace(existing) 
+    } else {
+      where <- ifelse(object@id==0L, "dataset", object %>% getName())
+      stop(sprintf("Element '%s' already exists in %s and has different properties. Amounts cannot be added.",
+                   x %>% getName(), where))
+    }
+  }
   return(object)
 })
 
