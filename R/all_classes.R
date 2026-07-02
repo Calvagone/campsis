374c1c539ddd709034e20dde972ad035884ef699
luyckxn <nicolas.luyckx@calvagone.com>
2026-07-02 18:34:40 +0200

    #202 Fix warning.

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,11 +1,15 @@
 #' Standard Campsis table class
 #'
-#' @name std_campsis_tbl
+#' @name std_campsis_tbl-class
+#' @aliases std_campsis_tbl
+#' @docType class
 #' @exportClass std_campsis_tbl
 setOldClass(c("std_campsis_tbl", "campsis_tbl", "tbl_df", "data.frame"))
 
 #' Campsis table class (see this class as an interface)
 #'
-#' @name campsis_tbl
+#' @name campsis_tbl-class
+#' @aliases campsis_tbl
+#' @docType class
 #' @exportClass campsis_tbl
 setOldClass(c("campsis_tbl", "tbl_df", "data.frame"))
