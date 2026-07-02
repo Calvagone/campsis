92d2aa7a6e008621fcfa97715bfb7254aa1bfef6
luyckxn <nicolas.luyckx@calvagone.com>
2026-07-02 18:14:21 +0200

    #202 Export campsis_tbl and std_campsis_tbl

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,1 +1,11 @@
+#' Standard Campsis table class
+#'
+#' @name std_campsis_tbl
+#' @exportClass std_campsis_tbl
 setOldClass(c("std_campsis_tbl", "campsis_tbl", "tbl_df", "data.frame"))
+
+#' Campsis table class (see this class as an interface)
+#'
+#' @name campsis_tbl
+#' @exportClass campsis_tbl
+setOldClass(c("campsis_tbl", "tbl_df", "data.frame"))
