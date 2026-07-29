30b1d342cd59c90dc2bc57d4c99af391b2d33ff0
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2026-07-29 14:23:54 +0200

    #209 Review vignettes 10 to 16.

--- vignettes/resources/Targets_Squeletton/_targets.R
+++ vignettes/resources/Targets_Squeletton/_targets.R
@@ -4,10 +4,10 @@ source("R/my_script.R")
 
 # Packages loaded by targets
 packages <- c("campsis", "progressr")
-tar_option_set(packages=packages)
+tar_option_set(packages = packages)
 
 # Enable progress bar
-options(progressr.enable=TRUE)
+options(progressr.enable = TRUE)
 progressr::handlers(campsis::campsis_handler())
 
 list(
@@ -17,10 +17,10 @@ list(
   ),
   tar_target(
     dataset,
-    makeDataset(subjects=1000, dose=1000)
+    make_dataset(subjects = 1000, dose = 1000)
   ),
   tar_target(
     results,
-    runSimulation(model=model, dataset=dataset, replicates=3)
+    runSimulation(model = model, dataset = dataset, replicates = 3)
   )
 )
--- vignettes/resources/Targets_Squeletton_Parallel/R/my_script.R
+++ vignettes/resources/Targets_Squeletton_Parallel/R/my_script.R
@@ -1,13 +1,23 @@
-
-makeDataset <- function(subjects, dose) {
-  dataset <- Dataset(subjects=subjects) %>%
-    add(Infusion(time=0, amount=dose, compartment=1, ii=24, addl=6)) %>%
-    add(Observations(seq(0, 7*24)))
+make_dataset <- function(subjects, dose) {
+  dataset <- Dataset(subjects = subjects) %>%
+    add(Infusion(
+      time = 0,
+      amount = dose,
+      compartment = 1,
+      ii = 24,
+      addl = 6
+    )) %>%
+    add(Observations(seq(0, 7 * 24)))
   return(dataset)
 }
 
-runSimulation <- function(model, dataset, replicates, seed=1) {
-  results <- with_progress(simulate(model=model, dataset=dataset, dest="mrgsolve",
-                                    seed=seed, replicates=replicates))
+run_simulation <- function(model, dataset, replicates, seed = 1) {
+  results <- with_progress(simulate(
+    model = model,
+    dataset = dataset,
+    dest = "mrgsolve",
+    seed = seed,
+    replicates = replicates
+  ))
   return(results)
 }
